4f5b0be3e33179e8bfdebd98da89820406c0b116
Kelly Bodwin <kbodwin@calpoly.edu>
2020-07-14 10:38:53 -0700

    more globals

--- R/globals.R
+++ R/globals.R
@@ -1,9 +1,10 @@
 # To avoid notes about internally consistent variables...
 
-utils::globalVariables(c("country", "state_abb", "state_name", "variable",
+utils::globalVariables(c("country", "county", "state_abb", "state_name", "variable",
                          "Letters", "NAME", "lon", "lat", "STATE",
                          "x", "y", "color", "window", "group",
                          "Date", "Info", "Address", "Des",
                          "Details", "Type", "Report1", "Report2", "Type1", "Type 12",
                          "Date/Time Occurred", "Occurance_Began", "Occurance_Ended",
-                         "Location", "Area", "Off_campus_Location", "Date_Time_Reported"))
+                         "Location", "Area", "Off_campus_Location", "Date_Time_Reported",
+                         "CAD", "Type 1"))
